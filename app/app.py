11b730ee9671438412a90745dec0f00ee0bcb73b
Guhan M <guhanmg25@gmail.com>
2026-01-30 19:52:10 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from flask import Response
+import time
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+    'app_requests_total',
+    'Total HTTP requests'
+)
+
+@app.route("/")
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello from monitored app 🚀"
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
